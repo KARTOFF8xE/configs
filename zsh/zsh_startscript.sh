330fed5a72f0b617fb84f3b0c8ff2b4de088a38f
KARTOFF8xE <georgmuck1@gmail.com>
2024-02-16 20:19:22 +0100

    fixed startscript

--- zsh/zsh_startscript.sh
+++ zsh/zsh_startscript.sh
@@ -11,27 +11,27 @@ if [ "$USER" != root ]; then
 fi
 set -e
 user=${1}
-cd /home/$user
+cd ~/
 ## install packages
 apt install zsh -y
 apt install exa -y
 apt install curl -y
 ## get .zshrc
-wget https://raw.githubusercontent.com/KARTOFF8xE/configs/main/zsh/.zshrc
+wget https://raw.githubusercontent.com/KARTOFF8xE/configs/main/zsh/.zshrc -O ~/.zshrc
 ## get vivid-Repo
 wget "https://github.com/sharkdp/vivid/releases/download/v0.8.0/vivid_0.8.0_amd64.deb" -O /tmp/vivid
 dpkg -i /tmp/vivid
 ## clone chromatic-zsh
-mkdir -p /home/$user/.zsh_scripts
-git clone https://github.com/jimmijj/chromatic-zsh.git /home/$user/.zsh_scripts/chromatic-zsh
+mkdir -p ~/.zsh_scripts
+git clone https://github.com/jimmijj/chromatic-zsh.git ~/.zsh_scripts/chromatic-zsh
 ## adding plugins
-mkdir -p /home/$user/.zsh_scripts/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$user/.zsh_scripts/plugins/zsh-autosuggestions
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh -P /home/$user/.zsh_scripts/plugins/
-source /home/$user/.zsh_scripts/plugins/git.plugin.zsh
+mkdir -p ~/.zsh_scripts/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh_scripts/plugins/zsh-autosuggestions
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh -O ~/.zsh_scripts/plugins/git.plugin.zsh
+zsh -c "source ~/.zsh_scripts/plugins/git.plugin.zsh"
 ## add history
-mkdir -p /home/$user/.cache/zsh
-touch /home/$user/.cache/zsh/history
+mkdir -p ~/.cache/zsh
+touch ~/.cache/zsh/history
 ## change login shell
 chsh $user -s $(which zsh)
 zsh
